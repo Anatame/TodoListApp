41cbc13ca04c9240e105c838830e719598f0bb16
Cinnybun <53411592+Cinnybun@users.noreply.github.com>
2020-09-26 14:47:27 +0530

    Updated port code

--- app.js
+++ app.js
@@ -240,12 +240,12 @@ app.get("/:customListName", function (req, res) {
 
 
 
-    let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+//     let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
+// app.listen(port);
 
-app.listen(port, function () {
+app.listen(process.env.PORT, function () {
     console.log("Server has started successfully!");
 });
